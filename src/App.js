9d5a7f522d34e9ed7f788db21a247763c8d9a8fa
jstirpe <jennstirpe@yahoo.com>
2022-06-15 14:39:54 -0400

    Set up themes and global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 
+import GlobalStyles from "./components/styled/Global";
+import { ThemeProvider } from 'styled-components';
+
+
+const theme = {
+  colors: {
+    primary: {
+      active: 'rgb(58, 123, 253)',
+      checkBg: 'linear-gradient rgb(87, 221, 255) to rgb(192, 88, 243)',
+    },
+    lightMode: {
+      bgMain: 'rgb(250, 250, 250)',
+      bgTasks: 'rgb(255, 255, 255)',
+      mainText: 'rgb(147, 148, 165)',
+      tasksText: 'rgb(72, 75, 106)',
+      borders: 'rgb(228, 229, 241)',
+      complete: 'rgb(210, 211, 219)',
+      hoverText: 'rgb(72, 75, 106)',
+    },
+    darkMode: {
+      bgMain: 'rgb(22, 23, 34)',
+      bgTasks: 'rgb(37, 39, 60)',
+      mainText: 'rgb(119, 122, 146)',
+      tasksText: 'rgb(202, 205, 232)',
+      borders: 'rgb(57, 58, 76)',
+      complete: 'rgb(77, 80, 102)',
+      hoverText: 'rgb(228, 229, 241)',
+    }
+  },
+}
 
 function App() {
   return (
-    <>
-    
-    </>
+    <ThemeProvider theme={theme} >
+      <>
+      <GlobalStyles />
+      <h1>test</h1>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/styled/Global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+
+    body {
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+`
+
+export default GlobalStyles;
